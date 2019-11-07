324159bccdef8ed13278a2c52355cd59cdba5d38
krumilurfigur <gabrieloberg3@gmail.com>
2019-11-07 11:11:00 +0100

    lektion 7/11 - 2019 Start

--- src/stenspel.java
+++ src/stenspel.java
@@ -1,3 +1,23 @@
 import java.util.Scanner;
 
-public class stenspel
+public class stenspel {
+    public static void main (String [] args){
+        //scanner
+        Scanner key = new Scanner(System.in);
+
+        //spelar läge
+        System.out.println("would you like to play 1 or 2 players?");
+        int antalspel = key.nextInt();
+
+        //spelet
+        if (antalspel == 1){
+            System.out.println("You are now playing against computer!" + "\nPlease enter rock, papper or scissor" + "\n rock = 1" + "\n papper = 2" + "\n scissor = 3");
+            int val = key.nextInt();
+
+        }
+        else{
+
+        }
+
+    }
+}
